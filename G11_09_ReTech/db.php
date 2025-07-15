496782b0705cef64e554b79dfd23dd0f2b405f6b
bella2731 <biela.diplomate@gmail.com>
2025-07-15 16:59:06 +0800

    Update db.php

--- G11_09_ReTech/db.php
+++ G11_09_ReTech/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "Querty";      
 $password = "12345!";          
-$dbname = "report"; 
+$dbname = "p25_retech"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
